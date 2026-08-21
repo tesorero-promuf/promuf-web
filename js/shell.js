4cfee9fab9371c005e9466ee04d2e15d3f00c61a
Boecio Figueroa <bomusic755@gmail.com>
2026-08-21 17:01:35 -0400

    Fix numVE: no quitar puntos cuando no hay coma (3887.05 queda 3887.05, no 388705)

--- js/shell.js
+++ js/shell.js
@@ -8,7 +8,7 @@
 const $ = id => document.getElementById(id);
 let MODULOS = [];
 let CFG = {};
-const APP_VER = 'r24';
+const APP_VER = 'r25';
 
 const AVISO_TIPOS = {
   aviso:      { ico: '📢', clase: 'b-teal' },
